e8a6c212a0bd53bd8c18a55c999580baf0566e7d
MatiassContreras <MatiHD4K@outlook.com.ar>
2022-11-12 22:23:03 -0300

    Creando la variable de intervalo de tiempo 5000mls

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,4 @@ let slider = document.querySelector(".slider-contenedor")
 let sliderIndividual = document.querySelectorAll(".contenido-slider")
 let contador = 1;
 let width = sliderIndividual[0].clientWidth;
+let intervalo = 5000;
