4ed7a8d0847d22e7f55ef266727ebd48ad6c0aa3
MatiassContreras <MatiHD4K@outlook.com.ar>
2022-11-14 15:09:25 -0300

    .

---
+++ JS/reto.js
@@ -0,0 +1,10 @@
+function addElemento(input) {
+
+    if (input == "Hola") {
+        var capa = document.getElementById("capa");
+        var h1 = document.createElement('h1')
+        h1.innerHTML = "Ingrese parametros en los dos"
+        capa.appendChild(h1);
+    }
+
+}
